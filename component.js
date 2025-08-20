5b41e704c6143ec76e02bd6c0c9ea4ff644590f0
Akshaytunk <akshaytunk29@gmail.com>
2025-08-20 21:14:55 +0530

    change

--- component.js
+++ component.js
@@ -27,7 +27,7 @@
     `;
 
     function applyDefinitionQuery() {
-        var svcLyr = gMyWebmap.findLayerById('daed1167baed413a9e38f47ea81b0fab'); 
+        var svcLyr = gMyWebmap.findLayerById('3ddc5954d2d14281ba53eef8a58cf1d4'); 
         console.log("Layer is", svcLyr);
 
         if (!svcLyr) return;
